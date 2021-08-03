77ea9a49fc5f17629d177260cfaa9dc4ee0adfd8
Johnsonha801 <Johnsonha801@gmail.com>
2021-08-02 20:38:48 -0600

    added new enemys and logged enemy names

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,22 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
 
 // fight function
 var fight = function() {
@@ -67,4 +80,4 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+//fight();
